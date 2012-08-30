294a92a898c9392727275c792d307d42b4c46b06
Will Green <will@hotgazpacho.org>
2012-08-30 16:23:12 -0400

    Automate building and packaging

---
+++ build.bat
@@ -0,0 +1,1 @@
+ %WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe .\build\build.proj
--- src/Binda/Properties/AssemblyInfo.cs
+++ src/Binda/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Binda")]
 [assembly: AssemblyDescription("Smart WinForms Data Binding")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Michael Minton")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Binda")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.12.0")]
-[assembly: AssemblyFileVersion("0.0.12.0")]
+[assembly: AssemblyVersion("0.0.13.0")]
+[assembly: AssemblyFileVersion("0.0.13.0")]
