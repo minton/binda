a5169b5ae4f42c1b0985bd9f8a57eecbbc3c694f
Michael Minton <michaelcminton@gmail.com>
2017-09-05 12:51:26 -0400

    Use newest nunit.console runner

--- build.fsx
+++ build.fsx
@@ -13,7 +13,7 @@ let projectDescription = "Smart WinForms Data Binding"
 let debugDir = __SOURCE_DIRECTORY__ + @"\src\binda\bin\Debug\"
 let releaseDir = __SOURCE_DIRECTORY__ + @"\src\binda\bin\Release\"
 //Hard coded paths since f# via FAKE always returns x86 ProgramFiles
-let nunit = __SOURCE_DIRECTORY__ + @"\src\packages\NUnit.ConsoleRunner.3.2.0\tools\nunit3-console.exe"
+let nunit = __SOURCE_DIRECTORY__ + @"\src\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe"
 let packages = __SOURCE_DIRECTORY__ + @"\src\packages"
 let version = getBuildParamOrDefault "ver" "0.0.1"
 let testAssemblies = !! (@"src\binda.tests\bin\Debug\binda.tests.dll")
